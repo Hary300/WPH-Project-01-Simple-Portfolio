75fc4bfbd97fb96cb9150ca91c20248e05274399
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-04-20 00:26:43 +0700

    feat: add tablet responsive layout

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const hamburgerBtn = document.querySelector('.hamburger-button');
 const menu = document.querySelector('.menu');
+const btnContainer = document.querySelector('.btn-container');
 hamburgerBtn.addEventListener('click', function () {
   hamburgerBtn.classList.toggle('open');
+  btnContainer.classList.toggle('open');
   menu.classList.toggle('open');
 });
 
@@ -9,6 +11,7 @@ document.addEventListener('click', function (event) {
   const isHeader = event.target.closest('header');
   console.log(event.target);
   if (!isHeader) {
+    btnContainer.classList.remove('open');
     menu.classList.remove('open');
   }
 });
